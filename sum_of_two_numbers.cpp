34a1d68113fc2c66f6111d61573613170b9b9e98
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-10-19 23:50:51 +0530

    Add files via upload

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int m , int n){
+    return m+n;
+}
+
+int main(){
+    int m, n;
+    cout<< "Enter m and n : "<< endl;
+    cin>> m >> n ;
+
+    cout<< sum(m,n) << endl;
+    
+}
