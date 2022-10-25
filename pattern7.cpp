6817a352f18a148564002e8b937b520f5a2b369d
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-10-25 15:41:26 +0530

    Add files via upload

---
+++ pattern7.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int i = 1;
+    while (i <= 5)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout <<j;
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+}
