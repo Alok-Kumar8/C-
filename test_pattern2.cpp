100eb308184b5488b9c5c33f992d2ad066fa7a90
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-10-29 16:44:46 +0530

    Add files via upload

---
+++ test_pattern2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    int i=1;
+    while (i<=n){
+        int j=1;
+        while(j<=n-i+1){
+            cout<<j;
+            j++;
+        }
+        int stars=1;
+        while(stars<=i-1){
+            cout<<"*";
+            stars++;
+
+        }cout<<endl;
+        i++;
+    }
+}
