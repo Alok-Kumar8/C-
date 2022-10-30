67aa646bea3d19939add35dac74849a27c36a3bb
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-10-30 10:29:01 +0530

    Add files via upload

---
+++ triangular_pattern2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int i = 1;
+
+    while (i <= n)
+    {
+        int spaces = 1;
+        while (spaces <= n - i)
+        {
+            cout << " ";
+            spaces++;
+        }
+        int k = i;
+        int j = 1;
+        while (j <= i)
+        {
+            cout << k;
+            k++;
+            j++;
+        }
+
+        cout << endl;
+        i++;
+    }
+}
