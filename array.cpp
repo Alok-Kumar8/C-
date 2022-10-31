005fad6af90b59e22f2e6d48b160877c7f0da505
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-10-31 23:13:19 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[10];
+    int n;
+    cin>>n;
+    for(int i=0; i<n ;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n ;i++){
+
+    
+    cout<< a[i]<<endl;
+    }
+cout<< sizeof(int)<<endl;
+}
