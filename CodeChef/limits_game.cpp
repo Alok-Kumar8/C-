2c49a9e5d0872df28495bc8601c62802c1dd636e
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-11-18 14:24:37 +0530

    Add files via upload

---
+++ CodeChef/limits_game.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if((a == 0) && (b == 0) && (c == 0) && (d == 0)){
+        cout << "IN" << endl;
+    }
+    else{
+        cout << "OUT" << endl;
+    }
+}
