c94639c5dcbca6b17cc777c863f1b5496dbebe13
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-11-25 20:26:33 +0530

    Add files via upload

---
+++ CodeChef/low_battery.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x <= 15){
+            cout << "yes" << endl;
+        }
+        else{
+            cout << "no" << endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
