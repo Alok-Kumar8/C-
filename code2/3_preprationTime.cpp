7ea155f1c9ebba966f35d34188dba8cc0c05e266
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-11-29 14:06:26 +0530

    Add files via upload

---
+++ code2/3_preprationTime.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int m, n, k;
+        cin >> m >> n >> k;
+        if(n*k < m){
+            cout << "yes" << endl;
+        }
+        else{
+            cout << "no" << endl;
+        }
+	return 0;
+}
