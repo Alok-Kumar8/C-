e0bc17434c4b680c00d74c0dea81f6def6e8e226
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-11-30 15:53:51 +0530

    Add files via upload

---
+++ code2/iq.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+        int n;
+        cin >> n;
+        if(n + 7 > 170){
+            cout << "yes" << endl;
+        }
+        else{
+            cout << "no" << endl;
+        }
+    
+	// your code goes here
+	return 0;
+}
