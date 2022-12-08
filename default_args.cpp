e662f4dff7a513fa4443db3170647e1765485075
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-12-08 12:12:26 +0530

    Add files via upload

---
+++ default_args.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b, int c = 3, int d = 2){
+    return a + b + c + d;
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << sum(a, b)<< endl;
+    
+}
