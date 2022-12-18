addeb3a675a4b594176243c8d473d9ceeb0ae89b
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-12-18 10:52:30 +0530

    Add files via upload

---
+++ code2/Rccursion/removeX.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void removeX(char s[]){
+    if(s[0]=='\0'){
+        return;
+    }
+
+    if(s[0]!='x'){
+        removeX(s + 1);
+    }
+    else{
+        int i = 1;
+        for(; s[i]!='\0';i++){
+            s[i-1]=s[i];
+        }
+        s[i-1]=s[i];
+        removeX(s);
+    }
+}
+
+int main(){
+    char s[100];
+    cin >> s;
+    removeX(s);
+    cout << s << endl;
+}
