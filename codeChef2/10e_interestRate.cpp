1c81f3611b71ca7edee04ec719e4ff123bf672ec
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-12-21 14:21:23 +0530

    Add files via upload

---
+++ codeChef2/10e_interestRate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x, y;
+        cin >> x >> y;
+        if(x >= 2*y){
+            cout << "yes" << endl;
+        }else {
+            cout << "no"<< endl;
+        }
+    }
+	return 0;
+}
