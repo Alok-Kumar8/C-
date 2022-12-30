8fab7fd70eef6ea2c1670ebbf34f4d838a7e46ed
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2022-12-30 20:40:15 +0530

    Add files via upload

---
+++ codeChef2/4b_luckyFour.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int i;
+	    cin >> i;
+	    int count=0;
+	    for(int j=i;j>0;){
+	        if(j%10==4){
+	            count=count+1;
+	        }
+	        j=j/10;
+	    }
+	    cout << count<< endl;
+	}
+	return 0;
+}
+
+
+
+
