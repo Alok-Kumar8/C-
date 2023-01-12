e1fb94639fd28a780a557dd7ff88b8d876ceb72e
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-01-12 15:11:01 +0530

    Add files via upload

---
+++ codeChef2/7c_problemCategory.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x >= 1 && x < 100){
+            cout << "Easy" << endl;
+        }
+        else if(x >= 100 && x < 200){
+            cout << "Medium" << endl;
+        }
+        else if(x >= 200 && x <=300){
+            cout << "Hard" << endl;
+        }
+    }
+	return 0;
+}
