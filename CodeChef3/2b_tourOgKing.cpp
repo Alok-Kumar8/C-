33bf1229d89e416e936828c5c74661ae4e6d2828
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-01-21 21:06:14 +0530

    Add files via upload

---
+++ CodeChef3/2b_tourOgKing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        cout << 5*n + 7*m << endl;
+    }
+	return 0;
+}
