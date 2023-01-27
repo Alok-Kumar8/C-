04470e1966ffe52379ec5da57999e1632011e0f3
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-01-27 23:20:37 +0530

    Add files via upload

---
+++ CodeChef3/3b_brainSpeed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if(y > x){
+        cout << "yes" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
+	// your code goes here
+	return 0;
+}
