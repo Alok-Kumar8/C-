4bb788fe38224ed36e440b6ac844891e200285d6
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-01-31 21:57:33 +0530

    Add files via upload

---
+++ CodeChef3/4c_waitingTiime.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int k,x;
+        cin >> k >> x;
+        cout << 7*k - x << endl;
+    }
+	return 0;
+}
