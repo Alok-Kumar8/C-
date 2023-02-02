a07d34343b871f44ebc83fa196ba8a7443a0efb7
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-02-02 22:18:19 +0530

    Add files via upload

---
+++ CodeChef3/5a_creditRequirement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x, y, z;
+        cin >> x >> y >> z;
+        cout << 4*x + 2*y << endl;
+    }
+	return 0;
+}
