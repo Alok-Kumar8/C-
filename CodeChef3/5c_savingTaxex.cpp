8804d62aad8b8a7287efdb7e530234c8b0138714
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-02-03 21:50:20 +0530

    Add files via upload

---
+++ CodeChef3/5c_savingTaxex.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        cout << x - y << endl;
+    }
+	// your code goes here
+	return 0;
+}
