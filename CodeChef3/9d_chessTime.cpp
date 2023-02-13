fe2a559c4e22c3096c19612d33768fa57e17ba07
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-02-13 21:42:06 +0530

    Add files via upload

---
+++ CodeChef3/9d_chessTime.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << (60*n)/20 << endl;
+    }
+	// your code goes here
+	return 0;
+}
