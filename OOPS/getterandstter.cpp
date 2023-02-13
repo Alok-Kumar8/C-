fe2be446a5cc899a5bcf8c8a8849e39d382b42a0
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-02-13 21:39:03 +0530

    Add files via upload

---
+++ OOPS/getterandstter.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include "studentClass.cpp"
+
+int main(){
+    Student s10;
+    int x = 28;
+    s10.setRollNumber(x);
+    s10.displayRollNumber();
+
+}
