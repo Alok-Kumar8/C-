3529fe07edfec1238129a11e5f22eb53dcfae333
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-02-23 16:21:29 +0530

    Add files via upload

---
+++ codeChef4/3a_goodTurn.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x, y;
+        cin >> x >> y;
+        if(x+y > 6){
+            cout << "yes" << endl;
+        }
+        else{
+            cout << "no" << endl;
+        }
+    }
+	return 0;
+}
