dab2df258e55b2ed689bbe5c082195d6f878c9be
Alok-Kumar8 <115494843+Alok-Kumar8@users.noreply.github.com>
2023-03-03 22:57:27 +0530

    Add files via upload

---
+++ OOPS/dynamicarrayUse.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include "dynamicArray.cpp"
+
+int main(){
+    dynamicArray d1;
+
+    d1.add(10);
+    d1.add(20);
+    d1.add(30);
+    d1.add(40);
+    d1.add(50);
+    d1.add(60);
+
+    d1.print();
+
+}
