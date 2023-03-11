9d9febc303b984e016707acde8d86a877a44c25e
Alok kumar <115494843+Alok-Kumar8@users.noreply.github.com>
2023-03-11 15:04:39 +0530

    Add files via upload

---
+++ linkedList/reverseLLUse.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include "reverseLL.cpp"
+
+node* reverseLL(node *head){
+    if(head == NULL && head->next == NULL){
+        return head;
+    }
+
+    node *smallAns = reverseLL(head->next);
+    node *temp = smallAns;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+
+    temp->next = head;
+    head->next = NULL;
+    return smallAns;
+}
+
+node* takeInput(){
+    int data;
+    cin >> data;
+    node *head = NULL;
+    node *tail = NULL;
+    while(data != -1){
+        node *newNode = new node(data);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(node *head){
+    while(head != NULL){
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    node *head = takeInput();
+    head = reverseLL(head);
+    print(head);
+}
+
+
+
