34e993a673cb1792ea5d14b2df6cfd7993d2c1df
Alok kumar <115494843+Alok-Kumar8@users.noreply.github.com>
2023-03-25 23:21:42 +0530

    Add files via upload

---
+++ Stacks/multpleTemplateUse.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include "multipleTemplate.cpp"
+
+int main(){
+    Pair<int, double> p1;
+    p1.setX(100);
+    p1.setY(100.45);
+
+    cout << p1.getX() << " " << p1.getY() << endl;
+}
