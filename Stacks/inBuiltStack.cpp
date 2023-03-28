d559d60fa9fa9864c9f7fa1a2a22ffcef17b71ce
Alok kumar <115494843+Alok-Kumar8@users.noreply.github.com>
2023-03-29 01:21:23 +0530

    Add files via upload

---
+++ Stacks/inBuiltStack.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> p;
+    // s.push(10);
+    // s.push(20);
+    // s.push(30);
+    // s.push(40);
+    // s.push(50);
+    // s.push(60);
+    // s.push(70);
+    // cout << s.top()<< endl;
+    // s.pop();
+    // s.pop();
+    // cout << s.top() << endl;
+    // cout << s.empty() << endl;
+
+    string s = "{a+b[c*d+(a+s)]}";
+    int len = s.size();
+    for(int i = 0; i<len; i++){
+
+    }
+    cout << len << endl; 
+}
