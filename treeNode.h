0d7f3138ec1679c21e472eb3da91e254aaed2e91
Alok kumar <115494843+Alok-Kumar8@users.noreply.github.com>
2023-04-04 15:41:25 +0530

    Add files via upload

---
+++ treeNode.h
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+template <typename T>
+class treeNode{
+    public:
+    T data;
+    vector<treeNode<T>*> children;
+
+    treeNode(T data){
+        this->data = data;
+    }
+    
+    ~treeNode(){
+        for(int i = 0; i < children.size(), i++){
+            delete children[i];
+        }
+    }
+};
